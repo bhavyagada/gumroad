8291e2f1c4256a0fbfa5bf5ee5340d86e50e9760
bhavya <bsg@somaiya.edu>
2024-08-13 20:00:51 -0700

    Fix localStorage bug

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,13 +1,13 @@
 import { browser } from "$app/environment";
 import { writable } from "svelte/store";
 
-export const is_logged_in = writable((browser && JSON.parse(localStorage.auth)) || false);
-export const user_store = writable((browser && JSON.parse(localStorage.user)) || {});
+const initialAuth = browser && localStorage.auth ? JSON.parse(localStorage.auth) : false;
+const initialUser = browser && localStorage.user ? JSON.parse(localStorage.user) : {};
+
+export const is_logged_in = writable(initialAuth);
+export const user_store = writable(initialUser);
+
 if (browser) {
-  is_logged_in.subscribe((value) => {
-    localStorage.auth = JSON.stringify(value);
-  });
-  user_store.subscribe((value) => {
-    localStorage.user = JSON.stringify(value);
-  });
+  is_logged_in.subscribe((value) => (localStorage.auth = JSON.stringify(value)));
+  user_store.subscribe((value) => (localStorage.user = JSON.stringify(value)));
 }
